1a4a7cfb6e306237f9086e2125d494624cc155d9
Абдурахманов Зафаржон <110538209+AbdurakhmanovAziz@users.noreply.github.com>
2022-09-24 15:26:15 +0500

    month
    month

---
+++ java.js
@@ -0,0 +1,40 @@
+let month = +prompt('месяц')
+let january = 1
+let  february= 2
+let  march = 3
+let  april= 4
+let  may= 5
+let  june= 6
+let  july= 7
+let  august= 8
+let  september= 9
+let  october= 10
+let november = 11
+let december = 12
+if(month === 1){
+    alert('Зима')
+}else if(month === 2){
+    alert('Зима')
+}else if(month === 3){
+    alert('Весна')
+}else if(month === 4){
+    alert('Весна')
+}else if(month === 5){
+    alert('Весна')
+}   else if(month === 6){
+    alert('Лето')
+}   else if(month === 7){
+    alert('Лето')
+}   else if(month === 8){
+    alert('Лето')
+}   else if(month === 9){
+    alert('Осень')
+}   else if(month === 10){
+    alert('Осень')
+}   else if(month === 11){
+    alert('Осень')
+}   else if(month === 12){
+    alert('Зима')
+}else(
+    alert('Я такого не знаю')
+)
